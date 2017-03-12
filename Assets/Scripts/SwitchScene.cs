5e26aa83284dfe15b9a22dfd34bccff898c4daf4
nayanseth <nayantronix@gmail.com>
2017-03-12 02:52:40 -0700

    Opening Scene

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SwitchScene : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.timeSinceLevelLoad > 5f)
+        {
+           
+            SceneManager.LoadScene("Main"); 
+        }
+	}
+}
