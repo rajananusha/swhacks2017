c98e22a7fc71e3e1237a0a582d95018d752f7858
nayanseth <nayantronix@gmail.com>
2017-03-12 03:33:15 -0700

    Opening Scene, Audio (Working)

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(SceneChange());
+        
+    }
+
+
+    IEnumerator SceneChange()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("Main");
+    }
+}
+
